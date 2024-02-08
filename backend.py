fa24e96ba66129460e30403fcf71572875cbd7e6
Aadesh <aadeshshrestha306@gmail.com>
2024-02-08 13:00:52 +0545

    Added API to work on a website

--- backend.py
+++ backend.py
@@ -1,21 +1,26 @@
 from fastapi import FastAPI, File, UploadFile, HTTPException
 from fastapi.responses import JSONResponse
 from PIL import Image
-import numpy as np 
 import tensorflow as tf 
 import io
-import cv2 as cv
+from fastapi.middleware.cors import CORSMiddleware
 
-
+#create an API instance
 app = FastAPI(title='Dog Cat Classifier')
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_headers=["*"],
+)
 
+#load saved model
 model = tf.keras.models.load_model("classifier.h5", compile=False)
 
+#preproces uploaded image
 def preprocess_img(pet):
-    pet_array = np.array(pet)
-    print("Type of pet variable:", type(pet_array))
-    print("Shape of pet variable:", pet_array.shape)
     img_array = pet.resize((224, 224)) 
     img_array = tf.keras.utils.img_to_array(img_array)
     img_array = tf.expand_dims(img_array, axis=0)
@@ -26,7 +31,7 @@ def preprocess_img(pet):
 async def root():
     return {"message": "Hello World!"}
 
-
+#post method to upload image and predict classes
 @app.post("/upload-image/")
 async def upload_image(file: UploadFile):
     contents = await file.read()
